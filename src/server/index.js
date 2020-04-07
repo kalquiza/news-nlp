065d7af5dd4633a9037e9ec21d4df8e90ec5bb57
Kristoffer Alquiza <kalquiza@gmail.com>
2020-04-07 03:29:38 -0700

    Configure aylien API

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,18 @@ var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 
+const dotenv = require('dotenv');
+dotenv.config();
+
+// import and instantiate the aylien SDK
+var AYLIENTextAPI = require('aylien_textapi');
+
+// set aylien API crdentials
+var textapi = new AYLIENTextAPI({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+  });
+
 const app = express()
 
 app.use(express.static('dist'))
