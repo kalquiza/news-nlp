98ce75aeb16691de7cb7a7576f19c4bcecbcb009
Kristoffer Alquiza <kalquiza@gmail.com>
2020-04-08 20:13:24 -0700

    Update express server port
    Run express server on port 8081. Include mock api response for testing.

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,8 @@
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+var bodyParser = require('body-parser')
+var cors = require('cors')
 
 const dotenv = require('dotenv');
 dotenv.config();
@@ -15,6 +17,13 @@ var textapi = new AYLIENTextAPI({
   });
 
 const app = express()
+app.use(cors())
+// to use json
+app.use(bodyParser.json())
+// to use url encoded values
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
 app.use(express.static('dist'))
 
@@ -26,9 +35,15 @@ app.get('/', function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
 })
+
+var json = {
+    'title': 'test json response',
+    'message': 'this is a message',
+    'time': 'now'
+}
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
